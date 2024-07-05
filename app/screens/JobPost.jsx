01a58118680bc16ecb5ba6fea6c61a946efb748c
JonnyCodeMaster <jon.farmer@hotmail.co.uk>
2024-07-05 16:58:21 +0100

    creates screen structure for mystuff and adds code for MyStuffHouseholder and JobPost

---
+++ app/screens/JobPost.jsx
@@ -0,0 +1,123 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext';
+import { db, addDoc, collection } from 'firebase/firestore';
+
+const JobPost = () => {
+    const [jobId, setJobId] = useState(); // can be same as document id
+    // job_id: jobs.length ? jobs[0].job_id + 1 : 1,
+    const [completedAt, setcompletedAt] = useState(null); // default to 'null' serverTimestamp() Date.now()
+    const [createdAt, setCreatedAt] = useState(); // serverTimestamp() Date.now()
+    const [jobTitle, setJobTitle] = useState();
+    const [jobImgUrl, setJobImgUrl] = useState();
+    const [jobDescription, setJobDescription] = useState();
+    const [jobServiceCategoryName, setJobServiceCategoryName] = useState();
+    const [jobMaxBudget, setJobMaxBudget] = useState();
+    const [jobStatus, setJobStatus] = useState('Open'); // default to 'Open'
+    const {user, setUser} = useContext(UserContext);
+
+    const newJob = {
+        completed_at: completedAt,
+        created_at: serverTimestamp(),
+        job_description: jobDescription,
+        job_id: Date.now(),
+        job_img_url: jobImgUrl,
+        job_max_budget: jobMaxBudget,
+        job_status: jobStatus,
+        job_title: jobTitle,
+        service_category_name: jobServiceCategoryName,
+        user_id: user.user_id
+    }
+
+    const handlePostNewJob = async () => {
+        const jobDocRef = await addDoc(collection(db, "jobs"), newJob)
+    }
+
+  return (
+    <View>
+        <TextInput
+              style={styles.input}
+              placeholder="Service category"
+              placeholderTextColor="#aaaaaa"
+              onChangeText={(text) => setJobServiceCategoryName(text)}
+              value={jobServiceCategoryName}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+        <TextInput
+              style={styles.input}
+              placeholder="Job title"
+              placeholderTextColor="#aaaaaa"
+              onChangeText={(text) => setJobTitle(text)}
+              value={jobTitle}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+        <TextInput
+              style={styles.input}
+              placeholder="Job description"
+              placeholderTextColor="#aaaaaa"
+              onChangeText={(text) => setJobDescription(text)}
+              value={jobDescription}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+        <TextInput
+              style={styles.input}
+              placeholder="Job image"
+              placeholderTextColor="#aaaaaa"
+              onChangeText={(text) => setJobImgUrl(text)}
+              value={jobImgUrl}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+        <TextInput
+              style={styles.input}
+              placeholder="Maximum budget"
+              placeholderTextColor="#aaaaaa"
+              keyboardType="numeric"
+              onChangeText={(text) => setJobMaxBudget(text)}
+              value={jobMaxBudget}
+              underlineColorAndroid="transparent"
+              autoCapitalize="none"
+            />
+        <TouchableOpacity style={styles.button} onPress={() => handlePostNewJob()}>
+          <Text style={styles.buttonTitle}>Submit</Text>
+        </TouchableOpacity>
+      <Text>Post new Job</Text>
+    </View>
+  )
+}
+
+export default JobPost
+
+const styles = StyleSheet.create({
+    input: {
+        height: 48,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        overflow: "hidden",
+        backgroundColor: "white",
+        marginTop: 7,
+        marginBottom: 7,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16,
+      },
+      button: {
+        backgroundColor: "#6759FF",
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+      buttonTitle: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+      }
+})
---
+++ app/screens/MyStuffHouseholder.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Pressable, StyleSheet, Text, View, FlatList, Image } from "react-native";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { app } from "../../FirebaseConfig";
+import { UserContext } from "../contexts/UserContext";
+const db = getFirestore(app);
+
+console.log("Hello Earth");
+
+const MyStuffHouseholder = () => {
+  const [allJobs, setAllJobs] = useState([]);
+  const { user } = useContext(UserContext);
+
+  const fetchAllJobs = async () => {
+    try {
+      const q = query(collection(db, "jobs"), where("user_id", "==", user.user_id));
+      const querySnapshot = await getDocs(q);
+
+      const jobsList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      console.log(user);
+      console.log(user.uid);
+
+      setAllJobs(jobsList);
+    } catch (error) {
+      console.error("Error fetching jobs: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllJobs();
+  }, []);
+
+  
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>My Jobs</Text>
+
+      <Pressable style={styles.button} onPress={() => console.log("Post A Job pressed")}>
+        <Text style={styles.buttonText}>Post A Job</Text>
+      </Pressable>
+
+      <FlatList
+        data={allJobs}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+         console.log(item.job_img_url),
+
+          <View style={styles.jobContainer}>
+            <Image source={{ uri: item.job_img_url }} style={styles.jobImage} />
+            <View style={styles.jobDetails}>
+              <Text style={styles.jobTitle}>{item.job_title}</Text>
+              {/* <Text style={styles.jobDescription}>{item.job_description}</Text> */}
+              {/* <Text style={styles.serviceCategory}>{item.service_category_name}</Text> */}
+              <Text style={styles.jobStatus}>Job Status: {item.job_status}</Text>
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={<Text>No jobs found.</Text>}
+      />
+      
+    </View>
+    
+  );
+};
+
+export default MyStuffHouseholder;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  jobContainer: {
+    flexDirection: "row",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    marginVertical: 5,
+  },
+  jobImage: {
+    width: 60,
+    height: 60,
+    marginRight: 10,
+    borderRadius: 5,
+    backgroundColor: "#ddd",
+  },
+  jobDetails: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  jobTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 3,
+  },
+  jobDescription: {
+    fontSize: 14,
+    color: "#666",
+    marginBottom: 3,
+  },
+  serviceCategory: {
+    fontSize: 14,
+    color: "#444",
+    marginBottom: 3,
+  },
+  jobStatus: {
+    fontSize: 14,
+    color: "#777",
+  },
+});
